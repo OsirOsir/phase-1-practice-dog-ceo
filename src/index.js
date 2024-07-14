362064c2fe2c7346dcaedf26f4931d9cd5ee35ab
Philip osir Otieno <philiposir2@gmail.com>
2024-07-14 15:06:51 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+document.addEventListener("DOMContentLoaded", () => {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+  fetch(imgUrl)
+    .then(res => res.json())
+    .then(imageData => {
+      addImagesToDOM(imageData.message);
+    })
+});
+
+function addImagesToDOM(images) {
+  const imageContainer = document.getElementById('dog-image-container');
+  images.forEach(image => {
+    const imgElement = document.createElement("img");
+    imgElement.src = image;
+    imgElement.alt = "Dog Image";
+
+    imageContainer.appendChild(imgElement);
+  });
+}
